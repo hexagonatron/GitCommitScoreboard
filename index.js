6cfa8a2ffeb2b50ceeb84099878d826bb2748fc3
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-02-22 21:29:30 +1030

    Frustration

--- index.js
+++ index.js
@@ -1,25 +1,63 @@
 var people = [
     ["Ben", "hexagonatron"],
-    ["Trent", "trentstollery"],
-    ["Linus Torvalds", "torvalds"]
+    ["Trent", "trentstollery"]
 ];
 
-getContributions = (person) => {
-    var url = `https://api.github.com/users/${person[1]}/repos`
+//https://api.github.com/repos/hexagonatron/BootcampW2Activity/commits?author=hexagonatron&since=2020-01-01T00:00:00Z
+
+var today = new Date();
+
+var yearAgo = new Date();
+
+var weekAgo = new Date();
+
+weekAgo.setTime(today.getTime() - (1000* 60 * 60 * 24 * 7))
+
+yearAgo.setTime(today.getTime() - (1000 * 60 * 60 * 24 * 365));
+
+var yearAgoISO = yearAgo.toISOString();
+
+console.log(yearAgoISO);
+
+
+people.forEach(person => {
+
+    var gitUser = person[1];
+
+    var url = `https://api.github.com/users/${gitUser}/repos`;
 
     console.log(url);
 
-    const fetchUrl = fetch(url, {mode: 'no-cors'});
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        var commitCount = 0;
+        console.log(data);
+        var repoCount = data.length;
+        console.log(repoCount);
 
-    console.log(fetchUrl);
+        data.forEach(repo => {
 
-    fetchUrl.then( response => {
-        console.log(response);
-    }).catch(err => {
-        console.log(err);
-    });
-}
+            var repoName = repo.name;
 
-people.forEach(person => {
-    var contribs = getContributions(person);
+            var repoUrl = `https://api.github.com/repos/${gitUser}/${repoName}/commits?since=${yearAgoISO}&author=${gitUser}`
+
+            console.log(repoUrl);
+
+            fetch(repoUrl)
+            .then(repoResponse => repoResponse.json())
+            .then(function(repoData){
+                console.log(repoData);
+                commitCount += repoData.length;
+                console.log(commitCount);
+
+            })
+        })
+
+        console.log(commitCount);
+        return commitCount;
+    })
+    .then(function(count){
+        console.log(count);
+    })
 });
